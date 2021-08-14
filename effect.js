17ed44ebcb029b17459c5847b40a59fa3a00e65f
datbanggia27022002 <88937720+datbanggia27022002@users.noreply.github.com>
2021-08-14 22:32:54 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -7,10 +7,10 @@ var z=0;
 var vid=document.getElementById("section-video");
 var sound=document.getElementById("section-sound");
 var noiDung1="Chịu khó đọc hết nhé  đây là công sức ngày tháng vật vã mấy tháng nay mà bé Đạt đã mày mò để chúc mừng sinh nhật chị đấy.";
-var noiDung2="Hôm nay ngày 16/8/2021 , một ngày đầy nắng và gió !Đây cũng chính là ngày mà đúng 20 năm về trước một Thiên Thần mở mắt chào đời Để rồi 20 năm sau Thiên Thần ấy giờ đây đã trở thành 1 người con gái dễ thương, xinh xắn mà còn tốt bụng nữa chứ !!!";
-var noiDung3="Nhân Dịp Này em gửi lời chúc mừng sinh nhật đến chị, chúc chị luôn thành công và gặp nhiều may mắn trong cuộc sống nhé! Hí Hí!!!!";
-var noiDung4=" em cũng muốn viết nhiều lắm,nhưng mà em nhạt lắm nên chả nghĩ ra điều gì cả ý.";
-var noiDung5="Món quà này, riêng Nó chẳng có ý nghĩa gì cả, nhưng mà cũng là tấm lòng vô giá  của em gửi đến chị thôi :))). “Happy birthday to chị Thúy”";
+var noiDung2="Hôm nay ngày 16/8/2021 ,tất cả những người sinh ra vào ngày này 20 năm trước đều vô cùng tuyệt vời nhưng có lẽ đối với em chị là người tuyệt vời nhất đấy ";
+var noiDung3="Vì vậy nên ngày hôm nay hãy cho em xem nụ cười tuyệt vời trong ngày xinh nhật của chị nè ";
+var noiDung4=" Chúc mừng sinh nhật chị";
+var noiDung5="”";
 //var noiDung6="520 <3";
 var tocdo=70;
 function hieuungdanhchu(){
